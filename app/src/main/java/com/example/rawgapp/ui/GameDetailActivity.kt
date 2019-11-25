3368e37c4cc7af7468f097d628cec41feb504ff9
enofeb <eneszor95@gmail.com>
2019-11-25 16:02:13 +0100

    Initial commit:screens and some dependencies added

---
+++ app/src/main/java/com/example/rawgapp/ui/GameDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.example.rawgapp.ui
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rawgapp.R
+
+class GameDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_detail)
+    }
+}
---
+++ app/src/main/java/com/example/rawgapp/ui/GameListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.rawgapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rawgapp.R
+
+class GameListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_list)
+    }
+}
