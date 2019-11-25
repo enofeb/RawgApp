4317083c39cf774c17b569467890445cfcb52bfd
enofeb <eneszor95@gmail.com>
2019-11-25 23:16:55 +0100

    updated:GameListActivity recyclerview with data

--- app/src/main/java/com/example/rawgapp/ui/GameListActivity.kt
+++ app/src/main/java/com/example/rawgapp/ui/GameListActivity.kt
@@ -1,10 +1,16 @@
 package com.example.rawgapp.ui
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.example.rawgapp.AppController
 import com.example.rawgapp.R
-import com.example.rawgapp.data.repository.GameRepository
+import com.example.rawgapp.ui.adapter.GameAdapter
 import com.example.rawgapp.ui.base.BaseActivity
 import com.example.rawgapp.ui.viewmodel.GameListViewModel
 import javax.inject.Inject
@@ -13,16 +19,46 @@ class GameListActivity : BaseActivity() {
 
     override val TAG: String get() = GameListActivity::class.java.simpleName
 
+    @BindView(R.id.rvGameList)
+    lateinit var rvMonsterList: RecyclerView
+
     @Inject
     internal lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private lateinit var gameListViewModel: GameListViewModel
 
+    private lateinit var gameAdapter: GameAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_list)
-
         (applicationContext as AppController).appComponent.inject(this)
+        ButterKnife.bind(this)
 
+        initView()
+        initViewModel()
+
+    }
+
+    private fun initView() {
+        gameAdapter = GameAdapter(applicationContext)
+
+        rvMonsterList.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = gameAdapter
+        }
+
+    }
+
+    private fun initViewModel() {
+        gameListViewModel =
+            ViewModelProviders.of(this, viewModelFactory).get(GameListViewModel::class.java)
+
+        gameListViewModel.loadGames()
+
+        gameListViewModel.getGameList().observe(this, Observer {
+            gameAdapter.updateGameList(it!!)
+        })
     }
 }
--- app/src/main/java/com/example/rawgapp/ui/adapter/GameAdapter.kt
+++ app/src/main/java/com/example/rawgapp/ui/adapter/GameAdapter.kt
@@ -1,25 +1,35 @@
 package com.example.rawgapp.ui.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rawgapp.R
+import com.example.rawgapp.data.entity.GameEntity
 
 class GameAdapter(private val context: Context) :
     RecyclerView.Adapter<GameAdapter.GameViewHolder>() {
 
+    private var mGameList: MutableList<GameEntity> = mutableListOf()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_layout_game, parent, false)
+        return GameViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val gameEntity=mGameList[position]
+        holder.tvGameName.text=gameEntity.name
     }
 
-    override fun getItemCount(): Int {
-        return 31
+    override fun getItemCount()=mGameList.size
+
+    fun updateGameList(gameList:List<GameEntity>){
+        mGameList.clear()
+        mGameList=gameList.toMutableList()
+        notifyDataSetChanged()
     }
 
     class GameViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
